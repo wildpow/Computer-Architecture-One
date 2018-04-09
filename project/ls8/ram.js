dd210a2040e5996f77348c1693bfc7ea9fdd9b59
Aaron Wilder <element1028@gmail.com>
2018-04-09 12:23:23 -0700

    implemented missing featres in ram.js

--- project/ls8/ram.js
+++ project/ls8/ram.js
@@ -13,6 +13,7 @@ class RAM {
     write(MAR, MDR) {
         // !!! IMPLEMENT ME
         // write the value in the MDR to the address MAR
+        this.mem[MAR] = MDR;
     }
 
     /**
@@ -23,6 +24,7 @@ class RAM {
     read(MAR) {
         // !!! IMPLEMENT ME
         // Read the value in address MAR and return it
+        return this.mem[MAR];
     }
 }
 
